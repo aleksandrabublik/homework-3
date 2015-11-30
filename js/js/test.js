df7e89e4b402bfff3493f6689096ff5331592c10
aleksandrabublik <sashkabublik95@gmail.com>
2015-11-30 23:21:53 +0200

    Update test.js

--- js/js/test.js
+++ js/js/test.js
@@ -1,37 +1,80 @@
-	
-function result(){													
-	var res1 = 0;													
-	var res2 = 0;													
-	var res3 = 0;													
-	var res4 = 0;													
-	var res5 = 0;													
-	var total = 0;													
-	
-	if (testform.task1.value == "true")								
-		res1 = 1;													
-	if (testform.task2.value == "true")								
-		res2 = 1;													
-	if (testform.task3.value == "true")								
-		res3 = 1;													
-	for (var i = 0; i < 4; i++)										
-		if (testform.task4[i].checked)								
-			if (testform.task4[i].value == "true")					
-				res4 = res4 + 1;											
-	for (i = 0; i < 4; i++)											
-		if (testform.task5[i].checked)								
-			if (testform.task5[i].value == "true")					
-				res5 = res5 + 1;									
-			
-	total = res1 + res2 + res3 + res4 + res5;						
-																			
-	alert(	"Результат теста:" +									
-			"\n задание 1 — " + res1 + " б (из 1)" +				
-			"\n задание 2 — " + res2 + " б (из 1);" +				
-			"\n задание 3 — " + res3 + " б (из 1)" +				
-			"\n задание 4 — " + res4 + " б (из 2)" +				
-			"\n задание 5 — " + res5 + " б (из 2)" +		
-			"\n" +														
-			"\n Общий результат — " + total + " б (из 7)");			
-}																
-		
-
+function Result()                                                                                          	
+{                                                                                                          	
+																											
+	var ResultTaskOne = 0;                                                                                	
+	var ResultTaskTwo = 0;                                                                                	
+	var	ResultTaskThree = 0;                                                                              	
+	var ResultTaskFour = 0;                                                                               	
+	var ResultTaskFive = 0;                                                                               	
+	var TotalResult = 0;                                                                                   	
+	var PercentageOfReadiness = 0;                                                                        	
+	var i;                                                                                                 	
+																											
+	if (testform.task1.value == "ref /")                                                                   	
+	{                                                                                                      	
+		ResultTaskOne += 1;                                                                               	
+	}                                                                                                      	
+																											
+	if (testform.task2.value == "ol")                                                                      	
+	{                                                                                                      	
+		ResultTaskTwo += 1;                                                                               	
+	}                                                                                                      	
+																											
+	if (testform.task3.value == "meta /")                                                                  	
+	{                                                                                                      	
+		ResultTaskThree += 1;                                                                             	
+	}                                                                                                      	
+																											
+	for(i = 0; i < 4; i++)                                                                                 	
+	{                                                                                                      	
+		if (testform.task4[i].checked)                                                                     	
+		{                                                                                                  	
+			if (testform.task4[i].value == "td" || testform.task4[i].value == "groupcol")                  	
+			{                                                                                              	
+				ResultTaskFour += 1;                                                                      	
+			}                                                                                              	
+			else                                                                                           	
+			{                                                                                              	
+				ResultTaskFour -= 0.5;                                                                    	
+			}                                                                                              	
+		}                                                                                                  	
+	}                                                                                                      	
+	if (ResultTaskFour < 0)                                                                               	
+	{                                                                                                      	
+		ResultTaskFour = 0;                                                                               	
+	}                                                                                                      	
+																											
+	for(i = 0; i < 4; i++)                                                                                 	
+	{                                                                                                      	
+		if (testform.task5[i].checked)                                                                     	
+		{                                                                                                  	
+			if (testform.task5[i].value == "autocomplete" || testform.task5[i].value == "action")          	
+			{                                                                                              	
+				ResultTaskFive += 1;                                                                      	
+			}                                                                                              	
+			else                                                                                           	
+			{                                                                                              	
+				ResultTaskFive -= 0.5;                                                                    	
+			}                                                                                              	
+		}                                                                                                  	
+	}                                                                                                      	
+	if (ResultTaskFive < 0)                                                                               	
+	{                                                                                                      	
+		ResultTaskFive = 0;                                                                               	
+	}                                                                                                      	
+																											
+	TotalResult = ResultTaskOne + ResultTaskTwo + ResultTaskThree + ResultTaskFour + ResultTaskFive;  	
+	PercentageOfReadiness = ((TotalResult * 100) / 7).toFixed(2);                                         	
+																											
+	alert(	"Результат теста:" 			  +                                                                	
+			"\n 1-й вопрос — " 			  + ResultTaskOne 		   + " (1)" +                              	
+			"\n 2-й вопрос — " 			  + ResultTaskTwo 		   + " (1)" +                              	
+			"\n 3-й вопрос — " 			  + ResultTaskThree 	   + " (1)" +                              	
+			"\n 4-й вопрос — " 			  + ResultTaskFour 	   + " (2)" +                              	
+			"\n 5-й вопрос — " 			  + ResultTaskFive 	   + " (2)" +                              	
+			"\n"						  +                                                                	
+			"\n Общий результат — " 	  + TotalResult 		   + " (7)" +                              	
+			"\n"						  +                                                                	
+			"\n The percentage if readiness: " + PercentageOfReadiness + "%");                           	
+}                                                                                                          	
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
